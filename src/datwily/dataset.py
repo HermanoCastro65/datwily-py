1cf63aaa4dff256c6decc17766c59c7222a7c736
HermanoCastro65 <hermanocastro18@gmail.com>
2026-02-27 18:32:44 -0300

    feat: allow Dataset to load CSV from file path

--- src/datwily/dataset.py
+++ src/datwily/dataset.py
@@ -1,3 +1,11 @@
+import pandas as pd
+from pathlib import Path
+
 class Dataset:
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, source):
+        if isinstance(source, pd.DataFrame):
+            self.df = source
+        elif isinstance(source, (str, Path)):
+            self.df = pd.read_csv(source)
+        else:
+            raise TypeError("Unsupported data source")
